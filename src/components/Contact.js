bfcfa728d283bae74df901718053e583179187ba
Aldo Junaidi Pebry <aldothepro@gmail.com>
2022-06-19 13:23:09 +0700

    key lupa on mapping

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React, {Fragment}from 'react';
+import React from 'react';
 import classes from './Contact.module.css';
 
 const Card = (props) => {
@@ -8,29 +8,34 @@ const Card = (props) => {
 };
 
 const SingleContact = (props) => {
-    return <div className={classes.contact}>
-        <img src={props.photo} alt="cat"></img>
-        <div className={classes.information}>
-            <h4>{props.name}</h4>
-            <p>{props.phone}</p>
-            <p>{props.email}</p>
+    return <Card>
+        <div className={classes.contact}>
+            <img src={props.photo} alt="cat"></img>
+            <div className={classes.information}>
+                <h4>{props.name}</h4>
+                <p>{props.phone}</p>
+                <p>{props.email}</p>
+            </div>
         </div>
-    </div>
+    </Card>
 };
 
 const Contact = ({data: contacts}) => {
     let contactCtx;
     if(Array.isArray(contacts)){
-        contactCtx = contacts.map(contact => {
-            return <Card><SingleContact photo={contact.photo} name={contact.name} phone={contact.phone} email={contact.email}/></Card>
+        contactCtx = contacts.map((contact,i) => {
+            return <li key={i}>
+                <SingleContact photo={contact.photo} name={contact.name} phone={contact.phone} email={contact.email}/>
+            </li>
         })
     } else {
         const contact = contacts;
-        contactCtx = <Card><SingleContact photo={contact.photo} name={contact.name} phone={contact.phone} email={contact.email}/></Card>
+        const contactId = Math.random() * 10
+        contactCtx = <li key={contactId}><SingleContact id={contactId}photo={contact.photo} name={contact.name} phone={contact.phone} email={contact.email}/></li>
     };
-    return <Fragment>
-        {contactCtx}
-    </Fragment>
+    return <ul>
+            {contactCtx}
+        </ul>
 }
 
 export default Contact;
